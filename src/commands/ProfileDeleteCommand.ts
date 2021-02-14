a4b3f96eb9164535424b316e2b40504884ca6eba
Rafael Triantafillidis <botazz133@gmail.com>
2021-02-14 15:31:52 +0200

    Added profile delete functionality

--- src/commands/ProfileDeleteCommand.ts
+++ src/commands/ProfileDeleteCommand.ts
@@ -1,4 +1,8 @@
 import {Command} from "./Command";
+import {ConfigFile} from "../file/ConfigFile";
+import {DEFAULT_CONFIG_PATH} from "../tokens";
+import {Profile} from "../Profile";
+import {ProfileNameDoesNotExist} from "../exceptions/ProfileNameDoesNotExist";
 
 export class ProfileDeleteCommand extends Command {
     private readonly profileName: string;
@@ -9,7 +13,16 @@ export class ProfileDeleteCommand extends Command {
     }
 
     execute(): void {
-
+        const config: ConfigFile = new ConfigFile(DEFAULT_CONFIG_PATH);
+        const profiles: Map<string, Profile> = config.profiles;
+        for (let prof of profiles.values()) {
+            if (prof.name === this.profileName) {
+                profiles.delete(prof.id);
+                config.write();
+                return;
+            }
+        }
+        throw new ProfileNameDoesNotExist();
     }
 
 
