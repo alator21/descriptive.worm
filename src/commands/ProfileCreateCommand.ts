f19de9b54dee8fea0a6374ad848d757d7cbcb965
Rafael Triantafillidis <botazz133@gmail.com>
2021-02-28 23:20:12 +0200

    Use default names

--- src/commands/ProfileCreateCommand.ts
+++ src/commands/ProfileCreateCommand.ts
@@ -1,6 +1,11 @@
 import {Command} from "./Command";
 import {ProfileNameAlreadyExists} from "../exceptions/ProfileNameAlreadyExists";
-import {DEFAULT_CONFIG_PATH, DEFAULT_PROFILES_PATH} from "../tokens";
+import {
+	DEFAULT_CONFIG_PATH,
+	DEFAULT_PROFILE_ALIASES_NAME,
+	DEFAULT_PROFILE_PATHS_NAME, DEFAULT_PROFILE_STARTUP_COMMANDS_NAME, DEFAULT_PROFILE_STARTUPS_NAME,
+	DEFAULT_PROFILES_PATH
+} from "../tokens";
 import {ProfileFolder} from "../folder/ProfileFolder";
 import {PathsFile} from "../file/PathsFile";
 import {AliasesFile} from "../file/AliasesFile";
@@ -18,11 +23,6 @@ export class ProfileCreateCommand extends Command {
 		this.profileName = profileName;
 	}
 
-	/**
-	 * check if profile name exists
-	 * if it exists throw exception
-	 * create structure for the new profile
-	 */
 	execute(): void {
 		const config: ConfigFile = new ConfigFile(DEFAULT_CONFIG_PATH);
 		const profiles: Map<string, Profile> = config.profiles;
@@ -35,10 +35,10 @@ export class ProfileCreateCommand extends Command {
 		const folderName: string = `${DEFAULT_PROFILES_PATH}/${this.profileName}`;
 		this.createEmptyStructure(folderName);
 		const newProfile: Profile = Profile.create(this.profileName);
-		newProfile.updatePathsPath(new PathsFile(`${folderName}/paths.json`));
-		newProfile.updateAliasesPath(new AliasesFile(`${folderName}/aliases.json`));
-		newProfile.updateStartupPath(new StartupFile(`${folderName}/startup.json`));
-		newProfile.updateStartupCommandsPath(new StartupCommandsFile(`${folderName}/startup-commands.json`));
+		newProfile.updatePathsPath(new PathsFile(`${folderName}/${DEFAULT_PROFILE_PATHS_NAME}.json`));
+		newProfile.updateAliasesPath(new AliasesFile(`${folderName}/${DEFAULT_PROFILE_ALIASES_NAME}.json`));
+		newProfile.updateStartupPath(new StartupFile(`${folderName}/${DEFAULT_PROFILE_STARTUPS_NAME}.json`));
+		newProfile.updateStartupCommandsPath(new StartupCommandsFile(`${folderName}/${DEFAULT_PROFILE_STARTUP_COMMANDS_NAME}.json`));
 
 		config.addProfile(newProfile);
 		config.write();
@@ -51,16 +51,16 @@ export class ProfileCreateCommand extends Command {
 		}
 		profileFolder.touch();
 
-		const pathsFile: PathsFile = new PathsFile(`${folderName}/paths.json`, true);
+		const pathsFile: PathsFile = new PathsFile(`${folderName}/${DEFAULT_PROFILE_PATHS_NAME}.json`, true);
 		pathsFile.touch();
 
-		const aliasesFile: AliasesFile = new AliasesFile(`${folderName}/aliases.json`, true);
+		const aliasesFile: AliasesFile = new AliasesFile(`${folderName}/${DEFAULT_PROFILE_ALIASES_NAME}.json`, true);
 		aliasesFile.touch();
 
-		const startupFile: StartupFile = new StartupFile(`${folderName}/startup.json`, true);
+		const startupFile: StartupFile = new StartupFile(`${folderName}/${DEFAULT_PROFILE_STARTUPS_NAME}.json`, true);
 		startupFile.touch();
 
-		const startupCommandsFile: StartupCommandsFile = new StartupCommandsFile(`${folderName}/startup-commands.json`, true);
+		const startupCommandsFile: StartupCommandsFile = new StartupCommandsFile(`${folderName}/${DEFAULT_PROFILE_STARTUP_COMMANDS_NAME}.json`, true);
 		startupCommandsFile.touch();
 
 	}
