ce659d2f62466d77fbb6646be6bc62ce54a4dcf9
Rafael Triantafillidis <botazz133@gmail.com>
2021-02-28 23:15:33 +0200

    Added default names for the various entities

--- src/tokens.ts
+++ src/tokens.ts
@@ -3,3 +3,8 @@ export const BASE_PATH: string = '~/.dworm';
 export const DEFAULT_CONFIG_PATH: string = `${BASE_PATH}/config.json`;
 export const DEFAULT_PROFILES_PATH: string = `${BASE_PATH}/profiles`;
 export const STARTSH_PATH: string = `${BASE_PATH}/start.sh`;
+
+export const DEFAULT_PROFILE_PATHS_NAME:string = `paths`;
+export const DEFAULT_PROFILE_ALIASES_NAME:string = `aliases`;
+export const DEFAULT_PROFILE_STARTUPS_NAME:string = `startup`;
+export const DEFAULT_PROFILE_STARTUP_COMMANDS_NAME:string = `startup-commands`;
